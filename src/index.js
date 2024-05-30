9a1474d0daa499b7b040c469da1e867a664ce0e9
maloua <maloua@users.noreply.github.com>
2024-05-30 13:42:50 +0200

    add surf color schemes

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import '@shoelace-style/shoelace/dist/themes/light.css';
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 setBasePath('/build/shoelace');
 
+import './../css/main.css';
+
 import './button/index.js';
 import './icon/index.js';
