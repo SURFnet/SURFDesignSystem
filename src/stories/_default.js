b627e1af22588ae984c946269368e4e8fde47ac4
maloua <maloua@users.noreply.github.com>
2024-09-18 10:56:27 +0200

    update default

--- src/stories/_default.js
+++ src/stories/_default.js
@@ -1,41 +1,13 @@
 export default {
   component: 'surf-component',
-  // render({ label, ...props }) {
-  //   const attributes = Object
-  //     .entries(props)
-  //     .map(([key, value]) => value && `${ key }="${ value }"`)
-  //     .filter(Boolean)
-  //     .join(' ')
-    
-  //   return `<surf-component ${ attributes }>${label}</surf-component>`
-  // },
-  argTypes: {
-    label: {
-      control: 'text',
-      description: 'Label is passed as a child element.',
-    },
-    boolean: {
-      description: 'Boolean toggle.',
-      control: 'boolean',
-      table: {
-        defaultValue: { summary: false },
-      }
-    },
-    options: {
-      description: 'Select.',
-      control: 'radio',
-      options: ['primary', 'success', 'neutral', 'warning', 'danger'],
-      mapping: { primary: '' },
-      table: {
-        defaultValue: { summary: 'primary' },
-      }
-    },
+  render() {
+    return `
+      <surf-component></surf-component>
+    `
   },
+  argTypes: {},
 };
 
 export const Main = {
-  args: {
-    label: 'Text',
-    boolean: true,
-  }
+  args: {}
 };
