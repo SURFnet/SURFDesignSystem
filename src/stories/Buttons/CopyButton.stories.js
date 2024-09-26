fb7cf093d814a8e13ac0380bb525277331a248af
maloua <maloua@users.noreply.github.com>
2024-09-26 17:26:23 +0200

    copy button: add controls for basic value and labels

--- src/stories/Buttons/CopyButton.stories.js
+++ src/stories/Buttons/CopyButton.stories.js
@@ -1,14 +1,36 @@
+import { html } from 'lit';
+
 export default {
   title: 'Buttons/Copy Button',
-  component: 'sl-copy-button',
-  render() {
-    return `
-      <sl-copy-button value="Shoelace rocks!"></sl-copy-button>
-    `
+  render(args) {
+    return html`
+<sl-copy-button
+  value="${ args.value }"
+  copy-label="${ args['copy-label'] }"
+  success-label="${ args['success-label'] }"
+  error-label="${ args['error-label'] }"
+></sl-copy-button>`
   },
-  argTypes: {},
+  argTypes: {
+    'copy-label': {
+      control: 'text',
+      defaultValue: { summary: 'Copy' }
+    },
+    'success-label': {
+      control: 'text',
+      defaultValue: { summary: 'Copied' }
+    },
+    'error-label': {
+      control: 'text',
+      defaultValue: { summary: 'Failed' }
+    },
+  },
 };
 
 export const Main = {
-  args: {}
+  args: {
+    value: 'This text will be copied to the clipboard',
+  }
 };
+
+// TODO: add icons and examples copying from other elements
